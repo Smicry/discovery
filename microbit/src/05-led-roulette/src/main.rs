2174ad12d89ec4401a8af60fd3bbaeb830619477
Smicry <smicry@vip.qq.com>
2023-01-05 22:50:18 +0800

    light it up with NB

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,47 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let light_it_all = [
+        [
+            // N
+            [1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 1],
+            [1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 1],
+            [1, 0, 0, 0, 1],
+        ],
+        [
+            // B
+            [1, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0],
+        ],
+    ];
+    let mut n = 0;
+    loop {
+        n = n % 2;
+        // Show light_it_all for 1000ms
+        display.show(&mut timer, light_it_all[n], 1000);
+        // clear the display again
+        display.clear();
+        timer.delay_ms(1000_u32);
+        n = n + 1;
+    }
 }
